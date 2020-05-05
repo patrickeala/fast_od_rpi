beb1b6ef07d26b9a8c1753bac76968418f947577
Justine Aguilar <jasaguilar2015@gmail.com>
2020-05-06 05:06:30 +0800

    Added Mat to vector converter function

--- final_pipeline/build/cmake_install.cmake
+++ final_pipeline/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/pi/Documents/EEE190/camera_pipeline/final_pipeline
+# Install script for directory: /home/pi/Documents/EEE190/fast_od_rpi/final_pipeline
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/pi/Documents/EEE190/camera_pipeline/final_pipeline/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/pi/Documents/EEE190/fast_od_rpi/final_pipeline/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- final_pipeline/liveSSD_pipeline.cpp
+++ final_pipeline/liveSSD_pipeline.cpp
@@ -2,7 +2,27 @@
 #include <iostream>
 #include <raspicam/raspicam_cv.h>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/core/matx.hpp>
 using namespace std;
+
+vector<uint8_t> convert_mat(cv::Mat input, int height, int width,int channels)
+{
+  vector<uint8_t> output(height * width * channels);
+
+  for(int i = 0; i < height; i++){
+    int des_pos;
+    for(int j = 0; j < width; j++){
+      des_pos = (i * width + j) * channels;
+
+      cv::Vec3b intensity = input.at<cv::Vec3b>(i, j);
+      output[des_pos] = (uint8_t) intensity.val[2]; //R
+      output[des_pos+1] = (uint8_t)intensity.val[1]; //G
+      output[des_pos+2] = (uint8_t) intensity.val[0]; //B
+    }
+  }
+
+  return output;
+}
 
 int main (int argc, char **argv)
 {
@@ -10,8 +30,11 @@ int main (int argc, char **argv)
 	raspicam::RaspiCam_Cv Camera;
 	cv::Mat image;
   cv::Mat resized;
-  
-  
+  int height = 480;
+  int width = 640;
+  int channels = 3;
+
+
   //camera currently set to capture frames of BGR color matrix format
   Camera.set( cv::CAP_PROP_FORMAT, CV_8UC3 );
 
@@ -30,7 +53,7 @@ int main (int argc, char **argv)
   while(1){
     Camera.grab();
 		Camera.retrieve(image);
-    cv::resize(image, resized, cv::Size(640,480));
+    cv::resize(image, resized, cv::Size(width,height));
 		cv::imshow("Camera View",resized);
     char c = cv::waitKey(1);
     if(c == 27) break;
@@ -42,16 +65,18 @@ int main (int argc, char **argv)
   while(1){
     Camera.grab();
 		Camera.retrieve(image);
-    cv::resize(image, resized, cv::Size(640,480));
-    //To do: convert Mat resized to std::vector<uin8_t> to make compatible w/ tflite
-    
+    cv::resize(image, resized, cv::Size(width,height));
+
+    //Convert Mat resized to std::vector<uin8_t> to make compatible w/ tflite
+    vector<uint8_t> input = convert_mat(resized,height,width,channels);
+
     /**
         TFLITE INFERENCE METHOD CALL HERE.
     **/
-    
+
     //To do: Draw bounding boxes on resized
 		cv::imshow("Camera View",resized);
-    
+
     char c = cv::waitKey(1);
     if(c == 27) break;
   }
